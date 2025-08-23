a6f4f3039f69adf6f64bcaad393fc84d6dcb2418
saronmeles4 <saronmeles4@gmail.com>
2025-08-23 18:06:04 +0300

    Retriev librarian

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -27,3 +27,12 @@ def books_by_author(author_name):
             print("-", book.title)
     except Author.DoesNotExist:
         print(f"No author found with name '{author_name}'")
+
+
+def librarian_for_library(library_name):
+    try:
+        library = Library.objects.get(name=library_name)
+        librarian = Librarian.objects.get(library=library_name)
+        print(f"\nLirarian for {library.name}: {librarian.name}")
+    except (Library.DoesNotExist, Librarian.DoesNotExist):
+        print(f"No libraria found for library '{library_name}'")
