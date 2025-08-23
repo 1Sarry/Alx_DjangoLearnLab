595d517e039e11bee806981a22f2fabea8e64667
saronmeles4 <saronmeles4@gmail.com>
2025-08-23 17:56:07 +0300

    list all books in a library

--- Introduction_to_Django/LibraryProject/bookshelf/update.md
+++
@@ -1,8 +0,0 @@
-from bookshelf.models import Book
-book = Book.objects.get(id=1)
-book.title = "Nintheen eighty four"
-book.title
-book.save()
-
-#Output
-# <Book: Nineteen Eighty-Four by George Orwell (1949)>
--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -7,6 +7,17 @@ django.setup()
 
 from relationship_app.models import Author, Book, Library, Librarian
 
+def books_in_library(library_name):
+    try:
+        library = Library.objects.get(name=library_name)
+        books = Book.objects.all()
+        print(f"\nBooks in {library.name}:")
+        for book in books:
+            print("-", book.title)
+           
+    except Author.DoesNotExist:
+        print(f"No library found with the name '{library_name}'")
+
 def books_by_author(author_name):
     try:
         author = Author.objects.get(name=author_name)
