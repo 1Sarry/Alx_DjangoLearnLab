db9c88d1f5d3ac59d0f40ae2776bfc65919b34e2
saronmeles4 <saronmeles4@gmail.com>
2025-08-24 09:55:09 +0300

    fix issues

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 from . import views
-from .views import BookListView
+from .views import book_list, BookListView
 
 
 urlpatterns = [
-    path('book_list/', views.book_list),
-    path('book_list_detail/', BookListView.as_view(), name = 'book_list')
+    path('books/', book_list, name= 'book_list'),
+    path('library/', BookListView.as_view(), name = 'library')
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,16 +1,15 @@
 from django.shortcuts import render
-from .models import Book
+from .models import Book, Library
 from django.views.generic import ListView, DetailView
 # Create your views here.
 
 def book_list(request):
     books = Book.objects.all()
-    context = {'book_list': books}
-    return render(request, 'books/book_list.html', context)
+    return render(request, 'books/book_list.html', {'books': books})
 
 
 class BookListView():
    model = Book
    template = 'books/library_detail.html' 
-   context_object_name = 'book_list'
+   context_object_name = 'library'
 
