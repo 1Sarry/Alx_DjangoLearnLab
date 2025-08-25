38a0a88e17230017a1c709560f423eda065ccb5d
saronmeles4 <saronmeles4@gmail.com>
2025-08-25 13:53:43 +0300

    Separately import the permission decorator

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from django.contrib.auth.decorators import login_required, user_passes_test, permission_required
+from django.contrib.auth.decorators import login_required, user_passes_test
+from django.contrib.auth.decorators import permission_required
 from .models import Book
 from .models import Library
 from django.views.generic.detail import DetailView
