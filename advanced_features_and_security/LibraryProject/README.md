5468498d6ec167df58822ee915916e1240b3ddf3
saronmeles4 <saronmeles4@gmail.com>
2025-08-30 13:56:09 +0300

    documentation added

--- advanced_features_and_security/LibraryProject/README.md
+++ advanced_features_and_security/LibraryProject/README.md
@@ -1,1 +1,30 @@
 LibraryProject with Django
+
+# RBAC with Groups & Permissions in Django
+
+### Permissions
+
+Defined in `Article` model:
+
+- can_view
+- can_create
+- can_edit
+- can_delete
+
+### Groups
+
+- **Viewers** → [can_view]
+- **Editors** → [can_view, can_create, can_edit]
+- **Admins** → [can_view, can_create, can_edit, can_delete]
+
+### Usage
+
+- Permissions are enforced with @permission_required in `views.py`
+- Groups can be managed via Django Admin or `python manage.py setup_groups`
+- Assign users to groups → their permissions update automatically
+
+### Testing
+
+1. Create users.
+2. Assign them to groups.
+3. Log in and attempt restricted actions.
--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import permission_required
+from .models import Book
 from django.http import HttpResponse
 import datetime
 
@@ -7,3 +8,33 @@ def current_datetime(request):
     now = datetime.datetime.now()
     html = '<html lang="en"><body>It is now %s.</body></html>' % now
     return HttpResponse(html)
+
+
+@permission_required('bookshelf.can_view', raise_exception=True)
+def list_books(request):
+    books = Book.objects.all()
+    return render(request, 'relationship_app/list_books.html', {'books': books})
+@permission_required('your_app_name.can_create', raise_exception=True)
+def books_create(request):
+    if request.method == "POST":
+        title = request.POST.get("title")
+        content = request.POST.get("content")
+        Book.objects.create(title=title, content=content, created_by=request.user)
+        return redirect("article_list")
+    return render(request, "articles/create.html")
+
+@permission_required('bookshelf.can_edit', raise_exception=True)
+def books_edit(request, pk):
+    books = get_object_or_404(Book, pk=pk)
+    if request.method == "POST":
+        books.title = request.POST.get("title")
+        books.content = request.POST.get("content")
+        books.save()
+        return redirect("books_list")
+    return render(request, "books/edit.html", {"article": books})
+
+@permission_required('bookshelf.can_delete', raise_exception=True)
+def books_delete(request, pk):
+    books = get_object_or_404(Book, pk=pk)
+    books.delete()
+    return redirect("books_list")
