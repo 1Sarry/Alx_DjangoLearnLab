3af7f70fabf902bcfe6815c2101602b97a11169c
saronmeles4 <saronmeles4@gmail.com>
2025-09-04 17:43:37 +0300

    <int:pk>removed from update and delete paths, all CRUD urls added

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path, include
-from .views import CustomBookCreateView, CustomBookUpdateView, CustomBookDeleteView, CustomBookDetailView, CustomBookListView
+from .views import(CustomBookCreateView, CustomBookUpdateView, CustomBookDeleteView, CustomBookDetailView, CustomBookListView) 
 
 urlpatterns = [
     path('/books', CustomBookListView.as_view(), name='book-list-view'),
-    path('/books/<int:pk>/', CustomBookDetailView.as_view(), name='book-detail-view')
+    path('/books/<int:pk>/', CustomBookDetailView.as_view(), name='book-detail-view'),
+    path('/books/create/', CustomBookCreateView.as_view(), name='book-create-view'),
+    path('/books/update/', CustomBookUpdateView.as_view(), name='book-update-view'),
+    path('/books/delete/', CustomBookDeleteView.as_view(), name= 'book-delete-view')
 ]
