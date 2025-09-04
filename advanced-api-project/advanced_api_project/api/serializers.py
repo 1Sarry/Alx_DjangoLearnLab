8dc252dafb2b11f2db287bfdf0efed774c05609d
saronmeles4 <saronmeles4@gmail.com>
2025-09-04 13:19:45 +0300

    modles and serializers added to api app

---
+++ advanced-api-project/advanced_api_project/api/serializers.py
@@ -0,0 +1,24 @@
+from rest_framework import serializers
+from .models import Book, Author
+from datetime import date
+
+class BookSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Book
+        fields = '__all__'
+    
+    def validate_pub_year(self, value):
+        if value > date.today().year:
+            raise serializers.ValidationError("Publication can't be in the future! haha")
+        return value
+        
+
+class AuthorSerializer(serializers.ModelSerializer):
+
+    books = BookSerializer(many = True, read_only=True)
+    #Nested serializer
+    class Meta:
+        model = Author
+        fields =['name', 'books']
+    
+    
